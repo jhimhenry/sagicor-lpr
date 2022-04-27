362c61ad15cea86de24ba761e8f2912e5549e80c
jhimhenry <85653996+jhimhenry@users.noreply.github.com>
2022-04-27 13:01:17 -0500

    wrong things uploaded so changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sagicor-lpr';
+  selectedFile: File=null;
+
+  constructor (private http: HttpClient){}
+  onFileSelected(event) { 
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name); 
+    this.http.post('',fd {
+      reportProgress: true;
+      observe: 'events';
+    } );
+
+    .subscribe((events: any) => {
+      console.log(events);
+    });
+  }
 }
