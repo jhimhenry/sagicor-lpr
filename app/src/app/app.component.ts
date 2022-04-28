1775e87e0859100a0e85259a85ad1c8eaa8694d3
FireFoxHD <khamalipowell@gmail.com>
2022-04-27 22:30:07 -0500

    integrated backend and frontend

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private httpClient: HttpClient){}
+
+
+  onFileUpload(event : any){
+    const file = event.target.files[0];
+
+    if(!file) {
+      console.log("No file uploaded! ");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    const upload$ = this.httpClient.post("http://localhost:3000/", formData);
+    upload$.subscribe();
+
+  }
+
+}
---
+++ server/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express');
+const axios = require('axios').default;
+const multer = require('multer');
+const upload = multer({ dest: '../uploads' })
+const {uploadFile} = require('./s3Client')
+const cors = require('cors')
+const PORT = process.env.PORT
+let app = express();
+
+app.use(cors({ origin: '*'}));
+
+async function sendImageOpenalpr(imgUrl){
+  const secret_key = process.env.SECRET_KEY
+  let url = `https://api.openalpr.com/v3/recognize_url?image_url=${imgUrl}&recognize_vehicle=0&country=us&return_image=0&topn=10&secret_key=${secret_key}`;
+  
+  try{
+    const response = await axios.post(url);
+    let plateData = response.data.results[0];
+    return plateData;
+  }catch(err){
+    console.log(err);
+  }
+}
+
+app.post('/', upload.single('file'), async (req, res) => {
+  
+  const file = req.file
+
+  if(!file) return res.status(400).send("No file uploaded!")
+
+  console.log('originalname', file.originalname)
+  console.log('mimetype', file.mimetype)
+  console.log('path', file.path)
+  
+  let result = await uploadFile(file);
+  let url = result.Location
+  let plateData = await sendImageOpenalpr(url)
+
+  if(!plateData) return res.status(400).send("Error parsing plate!")
+
+  console.log(plateData)
+  return res.status(200).send('Success')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+})
